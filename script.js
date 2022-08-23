63cbb621fad53148953ef3104e5c2f6c7c75c054
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-08-23 11:31:50 +0300

    213. ES6 Classes

--- script.js
+++ script.js
@@ -67,3 +67,26 @@ Array.prototype.unique = function () { // my own method for all arrays
 };
 
 console.log(arr.unique()); // [3, 7, 8, 18, 11, 13]
+
+
+// 213. ES6 Classes
+
+// class expression
+const PersoneCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+ 
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica); // PersonCl {firstName: 'Jessica', birthYear: 1996}
+
